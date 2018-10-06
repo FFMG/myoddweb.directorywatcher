a44cdf0bce858c891c957e787c984d2d406c9cb2
FFMG <github@myoddweb.com>
2018-10-06 06:33:07 +0200

    Started work on .nuspec release

---
+++ build.bat
@@ -0,0 +1,19 @@
+@IF "%~1" == "" GOTO Error
+@IF "%~1" == "/t:Push" GOTO Push
+@IF "%~1" == "/t:Pack" GOTO Pack
+
+:Pack
+.\tools\nuget\Nuget.exe pack ".\src\myoddweb.directorywatcher\myoddweb.directorywatcher.nuspec" -OutputDirectory ".\src\bin\."
+
+@GOTO End
+
+:Push
+@echo You will need to push it manually or use
+@echo .\tools\nuget\Nuget.exe push .\src\bin\Myoddweb.DirectoryWatcher.0.0.1.0.nupkg <API KEY>
+
+@GOTO End
+
+:Error
+@echo Missing parametters, '/t:pack' or '/t:push'
+
+:End
--- src/myoddweb.directorywatcher/Properties/AssemblyInfo.cs
+++ src/myoddweb.directorywatcher/Properties/AssemblyInfo.cs
@@ -22,15 +22,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("fc3a40b9-2ebf-4142-b130-011738408399")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
