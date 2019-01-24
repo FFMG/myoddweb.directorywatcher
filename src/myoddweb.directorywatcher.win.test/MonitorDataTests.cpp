e3972fb28984bc1c13fe5a9aee78f3cd54305fc6
FFMG <github@myoddweb.com>
2019-01-24 22:13:57 +0200

    Fixed google tests

--- src/myoddweb.directorywatcher.win.test/MonitorDataTests.cpp
+++ src/myoddweb.directorywatcher.win.test/MonitorDataTests.cpp
@@ -1,52 +1,29 @@
 ﻿#include "pch.h"
 
-#include "../myoddweb.directorywatcher.win/monitors/MonitorData.h"
+#include "../myoddweb.directorywatcher.win/monitors/win/Data.h"
+#include "../myoddweb.directorywatcher.win/monitors/WinMonitor.h"
 
-using myoddweb::directorywatcher::MonitorData;
+using myoddweb::directorywatcher::win::Data;
+using myoddweb::directorywatcher::WinMonitor;
+using myoddweb::directorywatcher::Request;
 
-TEST(MonitorData, BufferLenghValueIsSaved) {
-  MonitorData md(100);
+TEST(Data, BufferLenghValueIsSaved) {
+  const Request r = { L"c:\\", true };
+  WinMonitor wm( 1, r );
+  Data md(wm, 100);
   ASSERT_EQ(100, md.BufferLength() );
 }
 
-TEST(MonitorData, BufferIsNullByDefault) {
-  MonitorData md(100);
+TEST(Data, BufferIsNullByDefault) {
+  const Request r = { L"c:\\", true };
+  WinMonitor wm(1, r);
+  Data md(wm, 100);
   ASSERT_EQ(nullptr, md.Buffer());
 }
 
-TEST(MonitorData, DirectoryHandleIsNullByDefault) {
-  MonitorData md(100);
+TEST(Data, DirectoryHandleIsNullByDefault) {
+  const Request r = { L"c:\\", true };
+  WinMonitor wm(1, r);
+  Data md(wm, 100);
   ASSERT_EQ(nullptr, md.DirectoryHandle());
-}
-
-TEST(MonitorData, DirectoryHandleIsSaved) {
-  MonitorData md(100);
-  auto x = 10;
-  md.DirectoryHandle( static_cast<void*>(&x));
-  ASSERT_EQ(static_cast<void*>(&x), md.DirectoryHandle());
-}
-
-TEST(MonitorData, ClearResetsDirectoryHandle) {
-  MonitorData md(100);
-  auto x = 10;
-  md.DirectoryHandle(static_cast<void*>(&x));
-  ASSERT_EQ(static_cast<void*>(&x), md.DirectoryHandle());
-  md.Clear();
-  ASSERT_EQ(nullptr, md.DirectoryHandle());
-}
-
-TEST(MonitorData, ClearResetsOverlaped) {
-  MonitorData md(100);
-  auto x = 10;
-  md.Prepare(&x);
-  ASSERT_EQ(static_cast<void*>(&x), md.Overlapped()->hEvent );
-  md.Clear();
-  ASSERT_EQ(nullptr, md.Overlapped()->hEvent );
-}
-
-TEST(MonitorData, ClearDoesNotChanedBufferLengh) {
-  MonitorData md(100);
-  ASSERT_EQ(100, md.BufferLength());
-  md.Clear();
-  ASSERT_EQ(100, md.BufferLength());
 }
