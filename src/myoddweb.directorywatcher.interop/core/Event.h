6e05dcafbea136b2d5d17042303881b308375a98
FFMG <github@myoddweb.com>
2019-11-09 14:05:33 +0200

    Fixed liscense header

--- src/myoddweb.directorywatcher.interop/core/Event.h
+++ src/myoddweb.directorywatcher.interop/core/Event.h
@@ -1,17 +1,6 @@
-﻿//This file is part of Myoddweb.Directorywatcher.
-//
-//    Myoddweb.Directorywatcher is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
-//
-//    Myoddweb.Directorywatcher is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with Myoddweb.Directorywatcher.  If not, see<https://www.gnu.org/licenses/gpl-3.0.en.html>.
+﻿// Licensed to Florent Guelfucci under one or more agreements.
+// Florent Guelfucci licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 #pragma once
 #include <string>
 
