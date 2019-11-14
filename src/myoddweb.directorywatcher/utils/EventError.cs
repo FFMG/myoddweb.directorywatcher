0c8c8e6763ca3c226cd74741f87f42a1559409fd
Florent Guelfucci <florent.guelfucci@derivco.co.za>
2019-11-14 09:12:43 +0200

    Made internal namespace visible to unit tests

--- src/myoddweb.directorywatcher/utils/EventError.cs
+++ src/myoddweb.directorywatcher/utils/EventError.cs
@@ -2,8 +2,10 @@
 // Florent Guelfucci licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 using System;
+using System.Runtime.CompilerServices;
 using myoddweb.directorywatcher.interfaces;
 
+[assembly: InternalsVisibleTo("myoddweb.directorywatcher.test")]
 namespace myoddweb.directorywatcher.utils
 {
   /// <inheritdoc />
