c993a7a39c8f5c26d5fac3f066a86b1f6e012ccf
Florent Guelfucci <florent.guelfucci@derivco.co.za>
2020-01-16 17:48:03 +0200

    Added load testing sample ... that does nothing just yet

---
+++ samples/myoddweb.directorywatcher.load/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using myoddweb.commandlineparser;
+
+namespace myoddweb.directorywatcher.load
+{
+  /// <summary>
+  /// Load testing to test various scenarios
+  /// Use "myoddweb.directorywatcher.load -help for details"
+  /// </summary>
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      var arguments = new CommandlineParser(args, new Dictionary<string, CommandlineData>
+      {
+        {"help", new CommandlineData{ IsRequired = false } }
+      });
+
+      // check for help
+      if (arguments.IsSet("help"))
+      {
+        DisplayHelp();
+        return;
+      }
+    }
+
+    private static void DisplayHelp()
+    {
+      Console.WriteLine("           myoddweb.directorywatcher.load");
+      Console.WriteLine("           ------------------------------");
+      Console.WriteLine("help............... Display this message." );
+    }
+  }
+}
