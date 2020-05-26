3dd1884730e8ddeb6a3c8410462e909c79c2d080
Florent Guelfucci <florent.guelfucci@derivco.co.za>
2020-05-26 07:18:13 +0200

    Updated some missing license headers

--- src/myoddweb.directorywatcher.win/utils/Threads/CallbackWorker.cpp
+++ src/myoddweb.directorywatcher.win/utils/Threads/CallbackWorker.cpp
@@ -1,5 +1,7 @@
+// Licensed to Florent Guelfucci under one or more agreements.
+// Florent Guelfucci licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 #include "CallbackWorker.h"
-
 #include <utility>
 
 namespace myoddweb::directorywatcher::threads
