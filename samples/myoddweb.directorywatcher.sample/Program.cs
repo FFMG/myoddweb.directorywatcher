1f3b188ff16a5ec55fbc8453008e5cae3859ee65
Florent Guelfucci <florent.guelfucci@derivco.co.za>
2020-07-02 17:44:24 +0200

    Added test for rename of folders.

--- samples/myoddweb.directorywatcher.sample/Program.cs
+++ samples/myoddweb.directorywatcher.sample/Program.cs
@@ -2,6 +2,7 @@
 // Florent Guelfucci licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 using System;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -26,12 +27,99 @@ namespace myoddweb.directorywatcher.sample
         // Console.WriteLine("Press Ctrl+C to stop to exit.");
         // WaitForCtrlC();
 
-        RunAndWait();
+        //watch.Add(new Request("z:\\", true, new Rates(10000, 0)));
+
+        // RunAndWait();
+
+        await RunAndRenameFolderInRoot().ConfigureAwait( false );
       }
       catch (Exception ex)
       {
         WriteException(ex);
       }
+    }
+
+    private static async Task RunAndRenameFolderInRoot()
+    {
+      using (var watch = new Watcher())
+      {
+        var drvs = System.IO.DriveInfo.GetDrives();
+        foreach (var drv in drvs)
+        {
+          if (drv.DriveType == DriveType.Fixed)
+          {
+            watch.Add(new Request(drv.Name, true, new Rates(50, 0)));
+          }
+        }
+        
+        //const string path = "z:\\";
+        //watch.Add(new Request(path, true, new Rates(50, 0)));
+
+        watch.OnLoggerAsync += async (e, token) =>
+        {
+          await Task.Yield();
+          Console.WriteLine($"[{DateTime.UtcNow:HH:mm:ss.ffff}]:{e.Message}");
+        };
+
+        // prepare the console watcher so we can output pretty messages.
+        var _ = new ConsoleWatch(watch);
+
+        // start watching
+        watch.Start();
+
+        var cts = new CancellationTokenSource();
+
+        // start the renaming of folder
+        const string path = "c:\\";
+        var task = RenameFolders( $"{path}\\{Guid.NewGuid()}", 1000, cts.Token );
+
+        Console.WriteLine("Press Ctrl+C to stop to exit.");
+        WaitForCtrlC();
+
+        // stop everything.
+        watch.Stop();
+
+        // only stop the task now
+        // in case errors are thrown durring the shutdown itself
+        cts.Cancel();
+
+        // wait for the task
+        await Task.WhenAll(task).ConfigureAwait(false);
+      }
+    }
+
+    private static async Task RenameFolders( string folder, int sleep, CancellationToken token )
+    {
+      // create the folder if needbe
+      if(!Directory.Exists(folder))
+      { 
+        //  if that throws then we might need to run as admin.
+        Directory.CreateDirectory(folder);
+      }
+
+      var folderFrom = folder;
+      var folderTo = $"{folder}.2";
+
+      while (true)
+      {
+        if (token.IsCancellationRequested)
+        {
+          break;
+        }
+
+        Console.WriteLine( $"Swapping {folderFrom} to {folderTo}.");
+        Directory.Move( folderFrom, folderTo );
+
+        // swap the 2
+        var t = folderFrom;
+        folderFrom = folderTo;
+        folderTo = t;
+
+        await Task.Delay(sleep, token).ConfigureAwait(false);
+      }
+
+      // try and clear all
+      Directory.Delete(folderFrom, true );
     }
 
     private static void RunAndWait()
@@ -46,7 +134,6 @@ namespace myoddweb.directorywatcher.sample
             watch.Add(new Request(drv.Name, true, new Rates(50, 0)));
           }
         }
-        //watch.Add(new Request("z:\\", true, new Rates(10000, 0)));
 
         // prepare the console watcher so we can output pretty messages.
         var _ = new ConsoleWatch(watch);
